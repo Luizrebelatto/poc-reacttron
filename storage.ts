64696e3cb75148638484eef3630b897b76b8d050
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-05-26 00:24:17 -0300

    refactor async methods

--- storage.ts
+++ storage.ts
@@ -1,7 +1,54 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export async function saveUserName(name: string) {
-  await AsyncStorage.setItem('@poc:userName', name);
+function isJson(value: string) {
+  try {
+    JSON.parse(value);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
-  console.tron.log('Nome salvo no AsyncStorage:', name);
+export async function getItemFromStorage(key: string){
+    try {
+        const value = await AsyncStorage.getItem(key)
+
+        if (!value) {
+            return null
+        }
+
+        if(isJson(value)){
+            return JSON.parse(value)
+        }
+        
+        return value;
+    } catch(error) {
+        console.tron.log(`getStorage Error: ${error}`)
+        return null;
+    }
+}
+
+type StorageValue = string | number | boolean | object | null;
+
+export async function setItemToStorage(key: string, value: StorageValue){
+    try {
+        const parsedValue = typeof value === "string" ? value : JSON.stringify(value);
+
+        await AsyncStorage.setItem(key, parsedValue);
+    } catch(error) {
+        console.tron.log(`setItemToStorage Error: ${error}`)
+    }
+}
+
+export async function saveUserName(name: string) {
+  await AsyncStorage.setItem('name', name);
+
+  console.tron.log('Name Saved', name);
+}
+
+export async function getUser(){
+    const name = await getItemFromStorage("name")
+    console.tron.log('Name read:', name);
+
+    return name;
 }
